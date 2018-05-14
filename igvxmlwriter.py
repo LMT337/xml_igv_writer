f5b282f9e80d67ab11a7d0d30826f1ca59e53aa6
Lee Trani <LMT337@gmail.com>
2018-05-14 16:18:44 -0500

    fixed printing issue

--- igvxmlwriter.py
+++ igvxmlwriter.py
@@ -20,35 +20,31 @@ def sample_data(file):
 
 def write_header(locus, outfile):
     cwd = os.getcwd()
-    with open(outfile, 'w') as outfilecsv:
-        outfile_writer = csv.writer(outfilecsv, delimiter='"', quoting=csv.QUOTE_NONE, escapechar=' ')
+    with open(outfile, 'w') as outf:
         header = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n<Session genome="b37" hasGeneTrack="true" ' \
                  'hasSequenceTrack="true" locus="{}" nextAutoscaleGroup="2" path="{}/{}" version="8">\n\t' \
                  '<Resources>'.format(locus, cwd, outfile)
-        outfile_writer.writerow([header])
+        outf.write(header)
     return
 
 
 def resources(outfile, bampath):
-    with open(outfile, 'a') as outfilecsv:
-        outfile_writer = csv.writer(outfilecsv, delimiter='"', quoting=csv.QUOTE_NONE, escapechar=' ')
-        resource = '\t\t<Resource path="{}"/>'.format(bampath)
-        outfile_writer.writerow([resource])
+    with open(outfile, 'a') as outf:
+        resource = '\n\t\t<Resource path="{}"/>'.format(bampath)
+        outf.write(resource)
     return
 
 
 def write_resource(outfile):
-    with open(outfile, 'a') as outfilecsv:
-        outfile_writer = csv.writer(outfilecsv, delimiter='"', quoting=csv.QUOTE_NONE, escapechar=' ')
-        resource_line = '\t</Resources>'
-        outfile_writer.writerow([resource_line])
+    with open(outfile, 'a') as outf:
+        resource_line = '\n\t</Resources>'
+        outf.write(resource_line)
     return
 
 
 def panel(outfile, bamfile, sample, common_name):
-    with open(outfile, 'a') as outfilecsv:
-        outfile_writer = csv.writer(outfilecsv, delimiter='"', quoting=csv.QUOTE_NONE, escapechar=' ')
-        panel = '\t</Panel>\n\t<Panel height="1850" name="Panel_{}" width="943">\n\t<Track altColor="0,0,178" ' \
+    with open(outfile, 'a') as outf:
+        panel = '\n\t</Panel>\n\t<Panel height="1850" name="Panel_{}" width="943">\n\t<Track altColor="0,0,178" ' \
                 'autoScale="true" color="175,175,175" colorScale="ContinuousColorScale;0.0;123.0;255,255,' \
                 '255;175,175,175" displayMode="COLLAPSED" featureVisibilityWindow="-1" fontSize="10" id="{}" name="{} '\
                 'Coverage" showReference="false" snpThreshold="0.2" sortable="true" visible="true">\n\t\t<DataRange ' \
@@ -62,14 +58,13 @@ def panel(outfile, bamfile, sample, common_name):
                 'sortable="true" visible="true">\n\t\t\t<RenderOptions/>\n\t\t</Track>'.format(sample, bamfile, sample,
                                                                                                bamfile, sample, bamfile,
                                                                                                sample, common_name)
-        outfile_writer.writerow([panel])
+        outf.write(panel)
     return
 
 
 def tail(outfile):
-    with open(outfile, 'a') as outfilecsv:
-        outfile_writer = csv.writer(outfilecsv,     delimiter='"', quoting=csv.QUOTE_NONE, escapechar=' ')
-        file_tail ='\t</Panel>\t<Panel height="102" name="FeaturePanel" width="943">\n\t\t<Track altColor="0,0,178" ' \
+    with open(outfile, 'a') as outf:
+        file_tail ='\n\t</Panel>\n\t<Panel height="102" name="FeaturePanel" width="943">\n\t\t<Track altColor="0,0,178" ' \
                    'autoScale="false" color="0,0,178" displayMode="COLLAPSED" featureVisibilityWindow="-1" ' \
                    'fontSize="10" id="Reference sequence" name="Reference sequence" sortable="false" visible="true"/>' \
                    '\n\t\t<Track altColor="0,0,178" autoScale="false" clazz="org.broad.igv.track.FeatureTrack" ' \
@@ -81,7 +76,7 @@ def tail(outfile):
                    'dividerFractions="0.010067114093959731,0.38926174496644295,0.8204697986577181"/>' \
                    '\n\t<HiddenAttributes>\n\t\t<Attribute name="DATA FILE"/>\n\t\t<Attribute name="DATA TYPE"/>' \
                    '\n\t\t<Attribute name="NAME"/>\n\t</HiddenAttributes>\n</Session>'
-        outfile_writer.writerow([file_tail])
+        outf.write(file_tail)
     return
 
 
